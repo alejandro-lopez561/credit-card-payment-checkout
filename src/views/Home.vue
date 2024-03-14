c97ac18e810f4da1d7ddcf92a46a8beb27f73936
Alejandro Lopez <alejandrolopez@192.168.1.4>
2024-03-14 00:56:10 -0500

    removed unnecessary files, created Home view component

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <h1>
+        {{ title }}
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello World",
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
