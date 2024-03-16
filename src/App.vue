3957c883dfe56139bd24ce18df62c666370c671e
Alejandro Lopez <alejandrolopez@192.168.1.4>
2024-03-16 15:11:57 -0500

    added base reset to App, removed Product component from App

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script setup>
 import Home from './views/Home.vue'
-import Product from './components/Product.vue'
+import "./sass/_base.scss";
 </script>
 
 <template>
   <Home />
-  <Product />
 </template>
-
-<style scoped>
-
-</style>
