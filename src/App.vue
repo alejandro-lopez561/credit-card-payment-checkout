96a59b9ba95c5ce2741d7a9ecb36d5fee803af4b
Alejandro Lopez <alejandrolopez@192.168.1.4>
2024-03-16 23:39:15 -0500

    added CrediCard to App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Home from './views/Home.vue'
 import Product from './components/Product.vue'
+import CreditCard from './components/CreditCard.vue'
 </script>
 
 <template>
   <Home />
   <Product />
+  <CreditCard />
 </template>
