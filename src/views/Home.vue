671ab07763502f8c9fa601038126b78a70917c85
Alejandro Lopez <alejandrolopez@192.168.1.4>
2024-03-16 23:53:45 -0500

    added CreditCard component to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <Product />
+  <CreditCard />
 </template>
 
 <script>
 import Product from '../components/Product.vue'
+import CreditCard from '../components/CreditCard.vue'
 
 export default {
   components: {
     Product,
+    CreditCard,
   },
 }
 </script>
